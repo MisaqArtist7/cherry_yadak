bfc366c6e9ace7646c148afe31dba778ed2db50e
MisaqArtist7 <misaqbabakhani777@gmail.com>
2026-07-09 10:29:01 +0330

    add order and orderItem table

--- app/(admin)/admin/users/page.tsx
+++ app/(admin)/admin/users/page.tsx
@@ -1,4 +1,7 @@
-export default function UserPage() {
+import prisma from "@/lib/prisma";
+export default async function UserPage() {
+
+    const users = await prisma.user.findMany()
 
     const usersSample = [
         { id: 1, name: "میثاق باباخانی", contact: "۰۹۱۲۳۴۵۶۷۸۹", email: "misagh@example.com", role: "مدیر کل", roleStyle: "bg-red-50 text-[#D92F4E]", date: "۱۴۰۲/۰۴/۱2", status: "فعال", statusStyle: "bg-emerald-50 text-emerald-700" },
@@ -19,7 +22,7 @@ export default function UserPage() {
                     <div className="flex items-center justify-between lg:block">
                         <div>
                             <h1 className="text-xl md:text-2xl font-black text-gray-900 tracking-tight">مدیریت کاربران</h1>
-                            <p className="text-xs md:text-sm text-gray-500 mt-1">مشاهده مشخصات، مدیریت نقش‌ها و تعیین وضعیت دسترسی کاربران سایت</p>
+                            <p className=" md: text-gray-500 mt-1">مشاهده مشخصات، مدیریت نقش‌ها و تعیین وضعیت دسترسی کاربران سایت</p>
                         </div>
                         
                         {/* دکمه همبرگری که فقط در موبایل و تبلت دیده می‌شود */}
@@ -39,7 +42,7 @@ export default function UserPage() {
                                 <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
                             </div>
                             <div>
-                                <span className="block text-xs md:text-sm text-gray-400 font-medium">کل کاربران</span>
+                                <span className="block  md: text-gray-400 font-medium">کل کاربران</span>
                                 <span className="text-lg md:text-xl font-black text-gray-900">۱,۲۴۰ نفر</span>
                             </div>
                         </div>
@@ -48,7 +51,7 @@ export default function UserPage() {
                                 <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                             </div>
                             <div>
-                                <span className="block text-xs md:text-sm text-gray-400 font-medium">کاربران فعال</span>
+                                <span className="block  md: text-gray-400 font-medium">کاربران فعال</span>
                                 <span className="text-lg md:text-xl font-black text-emerald-600">۱,۲۲۵ نفر</span>
                             </div>
                         </div>
@@ -57,7 +60,7 @@ export default function UserPage() {
                                 <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" /></svg>
                             </div>
                             <div>
-                                <span className="block text-xs md:text-sm text-gray-400 font-medium">حساب‌های مسدود</span>
+                                <span className="block  md: text-gray-400 font-medium">حساب‌های مسدود</span>
                                 <span className="text-lg md:text-xl font-black text-rose-600">۱۵ کاربر</span>
                             </div>
                         </div>
@@ -69,7 +72,7 @@ export default function UserPage() {
                             <input 
                                 type="search" 
                                 placeholder="جستجوی کاربر بر اساس نام، شماره یا ایمیل..." 
-                                className="w-full font-medium bg-gray-50 border border-gray-200/80 rounded-xl pr-11 pl-4 py-3 outline-none transition-all focus:border-[#D92F4E] focus:bg-white focus:ring-4 focus:ring-[#D92F4E]/5 text-sm text-gray-800 placeholder-gray-400"
+                                className="w-full font-medium bg-gray-50 border border-gray-200/80 rounded-xl pr-11 pl-4 py-3 outline-none transition-all focus:border-[#D92F4E] focus:bg-white focus:ring-4 focus:ring-[#D92F4E]/5  text-gray-800 placeholder-gray-400"
                             />
                             <svg className="w-5 h-5 absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -77,7 +80,7 @@ export default function UserPage() {
                         </div>
 
                         <div className="w-full md:w-48">
-                            <select className="w-full border border-gray-200 bg-gray-50 rounded-xl p-3 font-bold text-gray-600 text-sm outline-none focus:bg-white focus:border-[#D92F4E] focus:ring-4 focus:ring-[#D92F4E]/5 transition-all cursor-pointer appearance-none bg-[url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23a0aec0%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E')] bg-[length:10px_auto] bg-[position:left_14px_center] bg-no-repeat pl-10">
+                            <select className="w-full border border-gray-200 bg-gray-50 rounded-xl p-3 font-bold text-gray-600  outline-none focus:bg-white focus:border-[#D92F4E] focus:ring-4 focus:ring-[#D92F4E]/5 transition-all cursor-pointer appearance-none bg-[url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23a0aec0%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E')] bg-[length:10px_auto] bg-[position:left_14px_center] bg-no-repeat pl-10">
                                 <option value="">همه نقش‌ها</option>
                                 <option value="admin">مدیران</option>
                                 <option value="customer">مشتریان عادی</option>
@@ -88,34 +91,32 @@ export default function UserPage() {
                     {/* جدول لیست کاربران (با ساختار اسکرول افقی ایمن برای موبایل) */}
                     <div className="bg-white rounded-2xl border border-gray-100 shadow-sm overflow-hidden">
                         <div className="overflow-x-auto block w-full vertical-scrollbar-fix">
-                            <table className="w-full text-right border-collapse min-w-[800px]">
+                            <table className="w-full text-right border-collapse min-w-200">
                                 <thead>
-                                    <tr className="bg-gray-50/70 text-gray-500 font-bold text-xs uppercase tracking-wider border-b border-gray-100">
-                                        <th className="p-4 min-w-[160px]">نام و نام خانوادگی</th>
+                                    <tr className="bg-gray-50/70 text-gray-500 font-bold  uppercase tracking-wider border-b border-gray-100">
+                                        <th className="p-4 min-w-40">نام و نام خانوادگی</th>
                                         <th className="p-4">شماره تماس</th>
-                                        <th className="p-4">ایمیل</th>
                                         <th className="p-4 text-center">نقش کاربری</th>
                                         <th className="p-4 text-center">تاریخ عضویت</th>
                                         <th className="p-4 text-center">وضعیت حساب</th>
                                         <th className="p-4 text-center w-28">عملیات</th>
                                     </tr>
                                 </thead>
-                                <tbody className="divide-y divide-gray-100 font-medium text-sm text-gray-600">
-                                    {usersSample.map((user) => (
+                                <tbody className="divide-y divide-gray-100 font-medium  text-gray-600">
+                                    {users.map((user) => (
                                         <tr key={user.id} className="hover:bg-gray-50/40 transition-colors group">
-                                            <td className="p-4 font-bold text-gray-900">{user.name}</td>
-                                            <td className="p-4 font-mono text-xs text-gray-600 tracking-wider" dir="ltr">{user.contact}</td>
-                                            <td className="p-4 text-gray-500 font-mono text-xs">{user.email}</td>
+                                            <td className="p-4 font-bold text-gray-900">{user.fullName}</td>
+                                            <td className="p-4 font-mono  text-gray-600 tracking-wider" dir="ltr">{user.phone}</td>
                                             <td className="p-4 text-center">
-                                                <span className={`inline-block px-2.5 py-1 rounded-lg text-xs font-bold ${user.roleStyle}`}>
-                                                    {user.role}
+                                                <span className={`inline-block px-2.5 py-1 rounded-lg  font-bold ${user.role}`}>
+                                                    {user.role === 'ADMIN' ? 'ادمین' : 'مهمان' }
                                                 </span>
                                             </td>
-                                            <td className="p-4 text-center text-gray-400 text-xs font-semibold">{user.date}</td>
+                                            <td className="p-4 text-center text-gray-400  font-semibold">{user.date}</td>
                                             <td className="p-4 text-center">
-                                                <span className={`inline-flex items-center gap-1.5 px-2.5 py-1 rounded-full text-xs font-bold ${user.statusStyle}`}>
-                                                    <span className={`w-1.5 h-1.5 rounded-full ${user.status === 'فعال' ? 'bg-emerald-500' : 'bg-rose-500'}`}></span>
-                                                    {user.status}
+                                                <span className={`inline-flex items-center gap-1.5 px-2.5 py-1 rounded-full  font-bold ${user.isVerified}`}>
+                                                    <span className={`w-1.5 h-1.5 rounded-full ${user.isVerified === true ? 'bg-emerald-500' : 'bg-rose-500'}`}></span>
+                                                    {user.isVerified ? 'فعال' : 'غیر فعال'}
                                                 </span>
                                             </td>
                                             <td className="p-4 text-center">
---
+++ prisma/migrations/20260709065041_add_address_order/migration.sql
@@ -0,0 +1,57 @@
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('PENDING', 'PAID', 'PROCESSING', 'SHIPPED', 'DELIVERED', 'CANCELED');
+
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "province" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "postalCode" TEXT NOT NULL,
+    "fullAddress" TEXT NOT NULL,
+    "isDefault" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "addressId" INTEGER NOT NULL,
+    "status" "OrderStatus" NOT NULL DEFAULT 'PENDING',
+    "totalAmount" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrderItem" (
+    "id" SERIAL NOT NULL,
+    "orderId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" INTEGER NOT NULL,
+
+    CONSTRAINT "OrderItem_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_addressId_fkey" FOREIGN KEY ("addressId") REFERENCES "Address"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
